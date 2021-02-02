7218affb7c6611d8476bddca9357e920b127b1c8
Shashwat Gupta <shashwat satna@gmail.com>
2021-02-02 18:41:05 +0530

    add timer

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,15 +1,25 @@
-var countDownDate = new Date("July 5, 2020 16:00:00").getTime();
+var countDownDate = new Date("April 2, 2021 16:00:00").getTime();
 var countdownfunction = setInterval(function () {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("demo").innerHTML = days + "d " + '&nbsp;' + hours + "h " + '&nbsp;' +
-        minutes + "m " + '&nbsp;' + seconds + "s ";
-    if (distance < 0) {
-        clearInterval(countdownfunction);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-    }
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("demo").innerHTML =
+    days +
+    "d " +
+    "&nbsp;" +
+    hours +
+    "h " +
+    "&nbsp;" +
+    minutes +
+    "m " +
+    "&nbsp;" +
+    seconds +
+    "s ";
+  if (distance < 0) {
+    clearInterval(countdownfunction);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
 }, 1000);
